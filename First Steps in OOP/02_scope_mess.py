132b1361d16f1305dd2e6d33fb3d8b1b77775949
Teodor469 <lifetroll6@gmail.com>
2024-02-20 20:27:06 +0200

    Create 02_scope_mess.py

---
+++ First Steps in OOP/02_scope_mess.py
@@ -0,0 +1,22 @@
+x = "global"
+
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x  # Use nonlocal to modify the variable in the outer function's scope
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        global x  # Use global to modify the global variable
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+print(x)
+outer()
+print(x)
