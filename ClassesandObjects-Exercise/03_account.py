4e870302c6821692be1ea9bc6746f88c4b385433
Teodor469 <lifetroll6@gmail.com>
2024-02-26 19:07:43 +0200

    Create 03_account.py

---
+++ ClassesandObjects-Exercise/03_account.py
@@ -0,0 +1,24 @@
+class Account:
+
+
+    def __init__(self, id, name, balance = 0) -> None:
+        self.id = id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self.balance
+    
+    def debit(self, amount):
+        if self.balance < amount:
+            return "Amount exceeded balance"
+        
+        self.balance -= amount
+
+        return self.balance
+    
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+        
