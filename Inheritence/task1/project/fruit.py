4c386daf769bdac4a4bbd291ae2bdfba5b000d74
Teodor469 <lifetroll6@gmail.com>
2024-02-27 18:42:10 +0200

    .

--- Inheritence/task1/project/fruit.py
+++ Inheritence/task1/project/fruit.py
@@ -1,2 +1,10 @@
 import sys
 sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Inheritence\\task1\\project')
+
+from project.food import Food
+
+
+class Fruit(Food):
+    def __init__(self, name, expiration_date) -> None:
+        self.name = name
+        super().__init__(expiration_date)
