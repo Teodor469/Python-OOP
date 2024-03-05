08c5a43db88aea6f928622c0e8007455f575ff72
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:37 +0200

    Create soup.py

---
+++ Encapsulation-exercise/task4/project/food/soup.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.food.starter import Starter
+
+class Soup(Starter):
+    pass
