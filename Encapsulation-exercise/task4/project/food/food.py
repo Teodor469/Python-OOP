0a1b1b52f21d5a2a72363214002a0214fc9f2b70
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:31 +0200

    Create food.py

---
+++ Encapsulation-exercise/task4/project/food/food.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.product import Product
+
+class Food(Product):
+    def __init__(self, name, price, grams) -> None:
+        super().__init__(name, price)
+        self.__grams = grams
+
+
+    @property
+    def grams(self):
+        return self.__grams
