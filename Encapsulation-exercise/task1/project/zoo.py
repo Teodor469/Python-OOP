dd929e87d1a424d4f224da245183e6e46c3dfc8c
Teodor469 <lifetroll6@gmail.com>
2024-03-05 19:39:54 +0200

    Create zoo.py

---
+++ Encapsulation-exercise/task1/project/zoo.py
@@ -0,0 +1,110 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task1\\project')
+from project.animal import Animal
+from project.worker import Worker
+
+
+class Zoo:
+    def __init__(self, name, budget, animal_capacity, workers_capacity) -> None:
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals = []
+        self.workers = []
+
+
+    def add_animal(self, animal: Animal, price):
+        if self.__budget >= price and self.__animal_capacity > len(self.animals):
+            self.animals.append(animal)
+            self.__budget -= price
+            return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+        elif self.__animal_capacity > len(self.animals) and self.__budget < price:
+            return "Not enough budget"
+        
+        return "Not enough space for animal"
+    
+
+    def hire_worker(self, worker: Worker):
+        if self.__workers_capacity > len(self.workers):
+            self.workers.append(worker)
+            return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+        
+        return "Not enough space for worker"
+    
+
+    def fire_worker(self, worker_name):
+        try:
+            worker = [w for w in self.workers if w.name == worker_name][0]
+            self.workers.remove(worker)
+            return f"{worker_name} fired successfully"
+        except IndexError:
+            return f"There is no {worker_name} in the zoo"
+        
+
+    def pay_workers(self):
+        money_to_pay = sum([w.salary for w in self.workers])
+
+        if money_to_pay <= self.__budget:
+            self.__budget -= money_to_pay
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+        
+    
+    def tend_animals(self):
+        money_to_feed_animals = sum([a.money_for_care for a in self.animals])
+
+        if money_to_feed_animals <= self.__budget:
+            self.__budget -= money_to_feed_animals
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return f"You have no budget to tend the animals. They are unhappy."
+    
+
+    def profit(self, amount):
+        self.__budget += amount
+
+
+    def animals_status(self):
+        result = f"You have {len(self.animals)} animals\n"
+        lions = [a for a in self.animals if a.__class__.__name__ == "Lion"]
+        amount_of_lions = len(lions)
+        result += f"----- {amount_of_lions} Lions:\n"
+        for lion in lions:
+            result += f"{lion}\n"
+
+        tigers = [a for a in self.animals if a.__class__.__name__ == "Tiger"]
+        amount_of_tigers = len(tigers)
+        result += f"----- {amount_of_tigers} Tigers:\n"
+        for tiger in tigers:
+            result += f"{tiger}\n"
+
+        cheetahs = [a for a in self.animals if a.__class__.__name__ == "Cheetah"]
+        amount_of_cheetas = len(cheetahs)
+        result += f"----- {amount_of_cheetas} Cheetahs:\n"
+        for cheetah in cheetahs:
+            result += f"{cheetah}\n"
+
+        return result[:-1]
+    
+
+    def workers_status(self):
+        result = f"You have {len(self.workers)} workers\n"
+
+        keepers = [w for w in self.workers if w.__class__.__name__ == "Keeper"]
+        caretakers = [w for w in self.workers if w.__class__.__name__ == "Caretaker"]
+        vets = [w for w in self.workers if w.__class__.__name__ == "Vet"]
+
+        result += f"----- {len(keepers)} Keepers:\n"
+        for k in keepers:
+            result += f"{k}\n"
+
+        result += f"----- {len(caretakers)} Caretakers:\n"
+        for c in caretakers:
+            result += f"{c}\n"
+
+        result += f"----- {len(vets)} Vets:\n"
+        for v in vets:
+            result += f"{v}\n"
+
+
+        return result[:-1]
