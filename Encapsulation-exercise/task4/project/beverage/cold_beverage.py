6afba7d17b45be0eef29e192aa8645905827718a
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:22 +0200

    Create cold_beverage.py

---
+++ Encapsulation-exercise/task4/project/beverage/cold_beverage.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.beverage.beverage import Beverage
+
+
+class ColdBeverage(Beverage):
+    pass
