e082c48603c939b3cf848f191e5d1742b62aa66d
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:33 +0200

    Create main_dish.py

---
+++ Encapsulation-exercise/task4/project/food/main_dish.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.food.food import Food
+
+class MainDish(Food):
+    pass
