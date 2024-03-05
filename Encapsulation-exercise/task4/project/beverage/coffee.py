f8dff622bc99e87ea4661f3cd9277cd88befc761
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:19 +0200

    Create coffee.py

---
+++ Encapsulation-exercise/task4/project/beverage/coffee.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.beverage.hot_beverage import HotBeverage
+
+class Coffee(HotBeverage):
+    MILLILITERS = 50
+    PRICE = 3.50
+
+    def __init__(self, name, price, milliliters, caffeine) -> None:
+        super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
+        self.__caffeine = caffeine
+
+
+    @property
+    def caffeine(self):
+        return self.__caffeine
