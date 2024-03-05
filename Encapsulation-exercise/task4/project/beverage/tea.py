661665be60d1089bb2a19ba3efddadf90fd183f7
Teodor469 <lifetroll6@gmail.com>
2024-03-05 20:09:26 +0200

    Create tea.py

---
+++ Encapsulation-exercise/task4/project/beverage/tea.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task4\\project')
+
+from project.beverage.hot_beverage import HotBeverage
+
+class Tea(HotBeverage):
+    pass
