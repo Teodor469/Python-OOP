12d426840d129a5e7e4535b61422d3d73ced4d6c
Teodor469 <lifetroll6@gmail.com>
2024-03-05 19:39:50 +0200

    Create vet.py

---
+++ Encapsulation-exercise/task1/project/vet.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-exercise\\task1\\project')
+
+from project.worker import Worker
+
+
+class Vet(Worker):
+    pass
