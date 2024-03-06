23f617488c894e244b0ff2f3c1c3e8cc26d1842a
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:57:09 +0200

    Update hot_beverage.py

--- Encapsulation-exercise/task4/project/beverage/hot_beverage.py
+++ Encapsulation-exercise/task4/project/beverage/hot_beverage.py
@@ -5,4 +5,5 @@ from project.beverage.beverage import Beverage
 
 
 class HotBeverage(Beverage):
-    pass
+    def __init__(self, name: str, price: float, milliliters: float):
+        super().__init__(name, price, milliliters)
