036f9a611da640868f404d67b1ba556260720bff
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:57:13 +0200

    Update cake.py

--- Encapsulation-exercise/task4/project/food/cake.py
+++ Encapsulation-exercise/task4/project/food/cake.py
@@ -3,10 +3,11 @@ sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-
 
 from project.food.dessert import Dessert
 
+
 class Cake(Dessert):
     GRAMS = 250
     CALORIES = 1000
     PRICE = 5
 
-    def __init__(self, name, price, grams, calories) -> None:
+    def __init__(self, name: str):
         super().__init__(name, Cake.PRICE, Cake.GRAMS, Cake.CALORIES)
