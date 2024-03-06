cf580dbcdc5fb3265a53081cf7485bc97a11263a
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:56:47 +0200

    Update beverage.py

--- Encapsulation-exercise/task4/project/beverage/beverage.py
+++ Encapsulation-exercise/task4/project/beverage/beverage.py
@@ -3,11 +3,11 @@ sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-
 
 from project.product import Product
 
+
 class Beverage(Product):
-    def __init__(self, name, price, milliliters) -> None:
+    def __init__(self, name: str, price: float, milliliters: float):
         super().__init__(name, price)
         self.__milliliters = milliliters
-
 
     @property
     def milliliters(self):
