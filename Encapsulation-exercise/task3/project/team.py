d240478e6b908119e7e66778d0b2d6e9f8933942
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:56:42 +0200

    Update team.py

--- Encapsulation-exercise/task3/project/team.py
+++ Encapsulation-exercise/task3/project/team.py
@@ -3,23 +3,20 @@ sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-
 from project.player import Player
 
 class Team:
-    def __init__(self, name, rating) -> None:
+    def __init__(self, name: str, rating: int):
         self.__name = name
         self.__rating = rating
         self.__players = []
 
-    
     def add_player(self, player: Player):
         if player in self.__players:
-            return f"Player {self.name} has already joined"
+            return f"Player {player.name} has already joined"
         self.__players.append(player)
-        return f"Player {self.name} joined team {self.__name}"
-    
+        return f"Player {player.name} joined team {self.__name}"
 
-    def remove_player(self, player_name):
-        try:
-            player = [p for p in self.__players if p.name == player_name][0]
-            self.__players.remove(player)
-            return player
-        except IndexError:
-            return f"Player {player_name} not found"
+    def remove_player(self, player_name: str):
+        for player in self.__players:
+            if player.name == player_name:
+                self.__players.remove(player)
+                return player
+        return f"Player {player_name} not found"
