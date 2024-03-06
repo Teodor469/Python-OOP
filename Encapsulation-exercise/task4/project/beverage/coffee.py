ba11eef22930dfeab86cf59c7f48f3d925b6c0d8
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:57:01 +0200

    Update coffee.py

--- Encapsulation-exercise/task4/project/beverage/coffee.py
+++ Encapsulation-exercise/task4/project/beverage/coffee.py
@@ -3,14 +3,14 @@ sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\Encapsulation-
 
 from project.beverage.hot_beverage import HotBeverage
 
+
 class Coffee(HotBeverage):
     MILLILITERS = 50
     PRICE = 3.50
 
-    def __init__(self, name, price, milliliters, caffeine) -> None:
+    def __init__(self, name: str, caffeine: float):
         super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
         self.__caffeine = caffeine
-
 
     @property
     def caffeine(self):
