e07d20e5097199a2c5633139548f32cd4c0b41a2
Teodor469 <lifetroll6@gmail.com>
2024-03-06 09:57:30 +0200

    Update product.py

--- Encapsulation-exercise/task4/project/product.py
+++ Encapsulation-exercise/task4/project/product.py
@@ -1,13 +1,11 @@
 class Product:
-    def __init__(self,name, price) -> None:
+    def __init__(self, name: str, price: float):
         self.__name = name
         self.__price = price
-
 
     @property
     def name(self):
         return self.__name
-    
 
     @property
     def price(self):
