a094f19174f2be383dc2d4223eaf9959d1052788
Teodor469 <lifetroll6@gmail.com>
2024-03-12 20:03:38 +0200

    Update print_books.py

--- SOLID/05_DIP/print_books.py
+++ SOLID/05_DIP/print_books.py
@@ -6,10 +6,24 @@ class Book:
 class Formatter:
     def format(self, book: Book) -> str:
         return book.content
+    
+
+class PaperFormatter:
+    def format(self, book):
+        return book.content[:4]
+
 
 
 class Printer:
-    def get_book(self, book: Book):
-        formatter = Formatter()
+    def get_book(self, book: Book, formatter):
         formatted_book = formatter.format(book)
         return formatted_book
+    
+
+base_formatter = Formatter()
+book = Book("Some book")
+paper_formatter = PaperFormatter()
+
+p = Printer()
+
+print(p.get_book(book, base_formatter))
