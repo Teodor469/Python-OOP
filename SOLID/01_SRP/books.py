f51a898dd8660a398565865bb37aeecf5c7bc827
Teodor469 <lifetroll6@gmail.com>
2024-03-12 18:51:43 +0200

    Update books.py

--- SOLID/01_SRP/books.py
+++ SOLID/01_SRP/books.py
@@ -8,6 +8,9 @@ class Book:
     def turn_page(self, page):
         self.page = page
 
+    def __repr__(self) -> str:
+        return f"Title {self.title} by {self.author}"
+
 
 class Library:
     def __init__(self, books) -> None:
@@ -31,13 +34,32 @@ class Library:
 
 
     def get_books_by_author(self, author):
-        return [b for b in self.books if b.author == author]
+        return [str(b) for b in self.books if b.author == author]
     
 
     def get_book_by_title(self, title):
-        books = [b for b in self.books if b.title == title]
+        books = [str(b) for b in self.books if b.title == title]
 
         if not books:
             raise ValueError("No such title")
         
         return books
+    
+
+if __name__ == "__main__":
+    book = Book("1", "a")
+    book2 = Book("2", "a")
+    book3 = Book("3", "b")
+
+    lib = Library([book, book2, book3])
+
+    print(lib.get_book_by_title("1"))
+    print(lib.get_book_by_author("a"))
+
+    book4 = Book("4", "c")
+
+    lib.add_book(book4)
+
+    print(lib.get_book_by_title("4"))
+    print(lib.get_books_by_author("5"))
+    print(lib.get_books_by_title("non"))
