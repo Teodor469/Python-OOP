fbf01d525521e999320de7656a94a36ce477a364
Teodor469 <lifetroll6@gmail.com>
2024-03-16 11:46:15 +0200

    Create summit_peak.py

---
+++ exam-preperation/task1/project/peaks/summit_peak.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\exam-preperation\\task1\\project')
+from project.peaks.base_peak import BasePeak
+
+
+class SummitPeak(BasePeak):
+    def __init__(self, name: str, elevation: int):
+        super().__init__(name, elevation)
+
+
+    def calculate_difficulty_level(self) -> int:
+        if self.elevation > 2500:
+            return 'Extreme'
+        elif self.elevation >= 1500 and self.elevation <= 2500:
+            return 'Advanced'
+        
+    
+    def get_recommended_gear(self) -> list:
+        return ["Climbing helmet", "Harness", "Climbing shoes", "Ropes"]
