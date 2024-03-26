03de58004e23b1713018abe540e4972f596624b3
Teodor469 <lifetroll6@gmail.com>
2024-03-26 18:07:18 +0200

    Create deep_sea_fish.py

---
+++ exam-preperation/02-Structure-Skeleton/project/fish/deep_sea_fish.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('C:\\Users\\lifet\\Documents\\GitHub\\Python-OOP\\exam-preperation\\02-Structure-Skeleton\\project')
+from project.fish.base_fish import BaseFish
+
+
+class DeepSeaFish(BaseFish):
+    TIME_TO_CATCH = 180
+
+    def __init__(self, name: str, points: float) -> None:
+        super().__init__(name, points, time_to_catch = self.TIME_TO_CATCH)
+
+
+    def fish_details(self):
+        return f"{type(self).__name__}: {self.name} [Points: {self.points}, Time to Catch: {self.time_to_catch} seconds]"
