efd71924743f5eb1321b03099f928491a68a4195
Teodor469 <lifetroll6@gmail.com>
2024-04-03 14:32:18 +0300

    Update female_robot.py

---
+++ exam-preperation/task6/project/robots/female_robot.py
@@ -0,0 +1,13 @@
+from project.robots.base_robot import BaseRobot
+
+
+class FemaleRobot(BaseRobot):
+
+    INITIAL_WEIGHT = 7
+
+    def __init__(self, name: str, kind: str, price: float) -> None:
+        super().__init__(name, kind, price, weight = self.INITIAL_WEIGHT)
+
+
+    def eating(self):
+        self.weight += 1
