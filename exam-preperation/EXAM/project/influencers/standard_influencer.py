09b75020b040c0f76726581340ea933326cf4179
Teodor469 <lifetroll6@gmail.com>
2024-04-06 15:55:41 +0300

    Update standard_influencer.py

---
+++ exam-preperation/EXAM/project/influencers/standard_influencer.py
@@ -0,0 +1,28 @@
+from project.campaigns.base_campaign import BaseCampaign
+from project.influencers.base_influencer import BaseInfluencer
+
+
+
+class StandardInfluencer(BaseInfluencer):
+    
+    INITIAL_PAYMENT_PERCENTAGE = 0.45
+
+    def __init__(self, username: str, followers: int, engagement_rate: float) -> None:
+        super().__init__(username, followers, engagement_rate)
+
+
+    def calculate_payment(self, campaign: BaseCampaign):
+        payment = campaign.budget * self.INITIAL_PAYMENT_PERCENTAGE
+
+        return payment
+    
+
+    def reached_followers(self, campaign_type: str):
+        if campaign_type == "HighBudgetCampaign":
+            reached_followers = int(self.followers * self.engagement_rate * 1.2)
+        elif campaign_type == "LowBudgetCampaign":
+            reached_followers = int(self.followers * self.engagement_rate * 0.9)
+        else:
+            raise ValueError("Invalid campaign type")
+
+        return reached_followers
